a238e3565af18cd96c12231bf086bcd9cd73fdb2
Dave Shield <david.shield@gmail.com>
2018-11-09 08:26:39 +0000

    Add additional model fields

--- server/controllers/salesorderitems.js
+++ server/controllers/salesorderitems.js
@@ -6,6 +6,13 @@ module.exports = {
       .create({
         salesOrderId: req.params.salesOrderId,  
         productId: req.body.productId,
+        orderQty: req.body.orderQty,
+        sentQuantity: req.body.sentQuantity,
+        invoiceQuantity: req.body.DataTypes.INTEGER,
+        unitPrice: req.body.unitPrice,
+        unitCost: req.body.unitCost,
+        unitTax: req.body.unitTax,
+        lineTotal: req.body.lineTotal
       })
       .then(salesOrderItem => res.status(201).send(salesOrderItem))
       .catch(error => res.status(400).send(error));
