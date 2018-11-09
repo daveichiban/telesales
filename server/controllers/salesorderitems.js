d5d773258dbd925d3f6deac69d0199f6456e43ac
Dave Shield <david.shield@bcpsoftware.com>
2018-11-09 17:11:57 +0000

    Amend orderQty error

--- server/controllers/salesorderitems.js
+++ server/controllers/salesorderitems.js
@@ -8,11 +8,11 @@ module.exports = {
         productId: req.body.productId,
         orderQty: req.body.orderQty,
         sentQuantity: req.body.sentQuantity,
-        invoiceQuantity: req.body.DataTypes.INTEGER,
+        invoiceQuantity: req.body.invoiceQuantity,
         unitPrice: req.body.unitPrice,
         unitCost: req.body.unitCost,
-        unitTax: req.body.unitTax,
-        lineTotal: req.body.lineTotal
+        unitTax: req.body.unitTax
+        //lineTotal: req.body.lineTotal
       })
       .then(salesOrderItem => res.status(201).send(salesOrderItem))
       .catch(error => res.status(400).send(error));
