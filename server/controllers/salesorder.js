26950098d48f64edcf55350e766f9984dce35e56
Dave Shield <david.shield@gmail.com>
2018-11-09 08:26:20 +0000

    Add List and retreive method

--- server/controllers/salesorder.js
+++ server/controllers/salesorder.js
@@ -1,4 +1,5 @@
 const SalesOrder = require('../models').SalesOrder;
+const SalesOrderItem = require('../models').SalesOrderItem;
 
 module.exports = {
   create(req, res) {
@@ -6,6 +7,7 @@ module.exports = {
       .create({
         salesOrderNumber: req.body.salesOrderNumber,
         statusId: req.body.statusId,
+        customerId: req.body.customerId,
         billToaddressId: req.body.billToaddressId,
         shipToAddressId: req.body.shipToAddressId,
         subTotal: req.body.subTotal,
@@ -15,6 +17,35 @@ module.exports = {
         shippingDate: req.body.shippingDate
       })
       .then(salesOrder => res.status(201).send(salesOrder))
+      .catch(error => res.status(400).send(error));
+  },
+  list(req, res) {
+    return SalesOrder
+      .findAll({
+        include: [{
+          model: SalesOrderItem,
+          as: 'orderLines',
+        }],
+      })
+      .then(salesOrder => res.status(200).send(salesOrder))
+      .catch(error => res.status(400).send(error));
+  },
+  retrieve(req, res) {
+    return SalesOrder
+      .findById(req.params.salesOrderId, {
+        include: [{
+          model: SalesOrderItem,
+          as: 'orderLines',
+        }],
+      })
+      .then(salesOrder => {
+        if (!salesOrder) {
+          return res.status(404).send({
+            message: 'Sales Order Not Found',
+          });
+        }
+        return res.status(200).send(salesOrder);
+      })
       .catch(error => res.status(400).send(error));
   },
 };
